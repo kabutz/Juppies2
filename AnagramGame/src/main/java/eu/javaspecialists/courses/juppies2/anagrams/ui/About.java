11e3c0740023c2b994b3d713e9e57e472e6ebc4a
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-07-06 08:08:56 +0300

    Changed "copyright" notice to Your Name

--- AnagramGame/src/main/java/eu/javaspecialists/courses/juppies2/anagrams/ui/About.java
+++ AnagramGame/src/main/java/eu/javaspecialists/courses/juppies2/anagrams/ui/About.java
@@ -69,7 +69,7 @@ public class About extends JDialog {
         copyrightTextArea.setEditable(false);
         copyrightTextArea.setLineWrap(true);
         copyrightTextArea.setRows(8);
-        copyrightTextArea.setText("Anagrams\n\nCopyright (c) 2003  Irritable Enterprises, Inc.");
+        copyrightTextArea.setText("Anagrams\n\nCopyright (c) 2020 Your Name");
         copyrightTextArea.setWrapStyleWord(true);
         copyrightTextArea.setBorder(null);
         copyrightTextArea.setFocusable(false);
